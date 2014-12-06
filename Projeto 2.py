9473608036a0b91ee32fd8671a72ef59bab25726
Bruno Sotto-Mayor <bruno.castro.machado@tecnico.ulisboa.pt>
2014-12-06 17:31:47 +0000

    update escreve_tabuleiro

--- Projeto 2.py
+++ Projeto 2.py
@@ -255,11 +255,12 @@ def escreve_tabuleiro(t): # Transformador de saida     #Bruno
 	   Se os argumentos nao forem validos gera um ValueError.'''
 	if e_tabuleiro(t) == False:
 		raise ValueError('escreve_tabuleiro: argumentos invalidos')
-	print ('[',t[(1,1)],']','[', t[(1,2)],']','[', t[(1,3)],']','[', t[(1,4)] ,']')
-	print ('[', t[(2,1)],']','[',t[(2,2)],']','[', t[(2,3)],']','[', t[(2,4)],']')
-	print ('[',t[(3,1)],']','[',t[(3,2)],']','[', t[(3,3)], ']','[',t[(3,4)],']')
-	print ('[', t[(4,1)],']','[', t[(4,2)],']','[', t[(4,3)],']', '[',t[(4,4)],']') 
-	print ('pontuacao:', t['pontuacao'])
+	else:
+		print ('[',t[(1,1)],']','[', t[(1,2)],']','[', t[(1,3)],']','[', t[(1,4)] ,']')
+		print ('[', t[(2,1)],']','[',t[(2,2)],']','[', t[(2,3)],']','[', t[(2,4)],']')
+		print ('[',t[(3,1)],']','[',t[(3,2)],']','[', t[(3,3)], ']','[',t[(3,4)],']')
+		print ('[', t[(4,1)],']','[', t[(4,2)],']','[', t[(4,3)],']', '[',t[(4,4)],']') 
+		print ('pontuacao:', t['pontuacao'])
 	
 	
 		
